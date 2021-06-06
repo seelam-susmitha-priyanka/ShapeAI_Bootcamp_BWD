3b07f9cbb50b04a7220b8af4995005465db99b60
seelam-susmitha-priyanka <83171535+seelam-susmitha-priyanka@users.noreply.github.com>
2021-06-06 22:55:08 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+ function Note(){
+   return (
+     
+    <div className="note">
+    <h1> JavaScript and React.js </h1>
+    <p>This is the best bootcamp for those who  are looking to learn Javascript  and React JS  from the scratch. </p>
+    
+    </div>
+  
+     
+     
+
+
+     
+   );
+  
+  }
+  export default Note;
